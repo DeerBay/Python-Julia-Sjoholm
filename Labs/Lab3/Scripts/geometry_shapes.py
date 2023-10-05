dc78542138fa1ed6843fd10c3d1f4f60a26d9929
DeerBay <juliamariasjoholm@gmail.com>
2023-10-05 20:26:51 +0200

    add operator overload

--- Labs/Lab3/Scripts/geometry_shapes.py
+++ Labs/Lab3/Scripts/geometry_shapes.py
@@ -1,5 +1,5 @@
 class GeometryShapes:
-    '''Has two subclasses, Circel, and Rectangel'''
+    '''Has two subclasses, Circel, and Rectangel'''t
     def __init__(self, x: (int|float), y:(int|float)):
         self.x = x
         self.y = y
@@ -10,7 +10,22 @@ class GeometryShapes:
     def __repr__(self):
         return f'"Shape"({self.x =}, {self.y=})'
     
-    def __eq__(self, other: object) -> bool:
-        return f"These geometrical forms have the same area" if self.area == other.area else f"These geometric shapes do not have the same area"
+    '''Operator overloading'''
+
+    def __eq__(self, other):
+        return self.area == other.area
+
+    def __ne__(self, other):
+        return self.area != other.area
     
+    def __gt__(self, other):
+        return self.area > other.area
     
+    def __lt__(self, other):
+        return self.area < other.area
+    
+    def __ge__(self, other):
+        return self.area >= other.area
+    
+    def __le__(self, other):
+        return self.area <= other.area
