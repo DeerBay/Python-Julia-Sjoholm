3a764f064abf82a0bcb5229af9126606bea71a18
DeerBay <juliamariasjoholm@gmail.com>
2023-10-07 10:12:24 +0200

    uppdate of area method

--- Labs/Lab3/Scripts/geometry_shapes.py
+++ Labs/Lab3/Scripts/geometry_shapes.py
@@ -1,5 +1,5 @@
 class GeometryShapes:
-    '''Has two subclasses, Circel, and Rectangel'''
+    '''Base class for geometrical shapes'''
     def __init__(self, x: (int|float), y:(int|float)):
         self.x = x
         self.y = y
@@ -33,19 +33,19 @@ class GeometryShapes:
     '''Operator overloading'''
 
     def __eq__(self, other):
-        return self.area == other.area
+        return self.calculate_area() == other.calculate_area()
 
     def __ne__(self, other):
-        return self.area != other.area
+        return self.calculate_area() != other.calculate_area()
     
     def __gt__(self, other):
-        return self.area > other.area
+        return self.calculate_area() > other.calculate_area()
     
     def __lt__(self, other):
-        return self.area < other.area
+        return self.calculate_area() < other.calculate_area()
     
     def __ge__(self, other):
-        return self.area >= other.area
+        return self.calculate_area() >= other.calculate_area()
     
     def __le__(self, other):
-        return self.area <= other.area
+        return self.calculate_area() <= other.calculate_area()
--- Labs/Lab3/Scripts/rectangel.py
+++ Labs/Lab3/Scripts/rectangel.py
@@ -1,4 +1,33 @@
 from geometry_shapes import GeometryShapes
 
 class Rectangel(GeometryShapes):
-    pass
+    def __init__(self, x: int | float, y: int | float, height: int|float, width: int|float):
+        super().__init__(x, y)
+        self.height = height
+        self.width = width
+
+    def calculate_area(self):
+        return self.height * self.width
+    
+    def circumference(self):
+        return self.height*2 + self.width*2
+
+    @property
+    def height(self):
+        return self._height
+    
+    @height.setter
+    def height(self, height: (int|float)):
+        if not isinstance(height, (int|float)):
+            raise TypeError(f"value of height can only be numeric.")
+        self._height = height
+
+    @property
+    def width(self):
+        return self._width
+    
+    @width.setter
+    def width(self, width: (int|float)):
+        if not isinstance(width, (int|float)):
+            raise TypeError(f"value of Width can only be numeric.")
+        self._width = width
