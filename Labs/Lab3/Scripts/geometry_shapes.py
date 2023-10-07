4653d94029dccb2a0cbdcbfc0e934295b80ac4ac
DeerBay <juliamariasjoholm@gmail.com>
2023-10-07 10:40:28 +0200

    uppdate error handling setter properties

--- Labs/Lab3/Scripts/geometry_shapes.py
+++ Labs/Lab3/Scripts/geometry_shapes.py
@@ -1,37 +1,43 @@
 class GeometryShapes:
     '''Base class for geometrical shapes'''
     def __init__(self, x: (int|float), y:(int|float)):
+        '''Initalize a position with given x and y coordinates for a geometrical shape'''
         self.x = x
         self.y = y
 
     def __str__(self):
+        '''Return a position for a gemetrical shape'''
         return f"The position for this geometrical shape is (x, y): ({self.x}, {self.y})."
 
     def __repr__(self):
+        '''Return a string for developers'''
         return f'"Shape"({self.x =}, {self.y=})'
     
     @property
     def x(self):
+        '''Getter of x'''
         return self._x   
    
     @x.setter
     def x(self, position: (int|float)):
+        '''Setter of x, with error handling'''
         if not isinstance(position, (int|float)):
             raise TypeError(f"Position of x can only be numeric.")
         self._x = position
 
     @property
     def y(self):
+        '''Getter of y'''
         return self._y
     
     @y.setter
     def y(self, position: (int|float)):
+        '''Setter of y, with error handling'''
         if not isinstance(position, (int|float)):
             raise TypeError(f"Position of y can only be numeric.")
         self._y = position
     
-    '''Operator overloading'''
-
+    '''Operator overloading for equal, not equal, greater than, less than, greater or equal and less or equal for calculated areas.'''
     def __eq__(self, other):
         return self.calculate_area() == other.calculate_area()
 
--- Labs/Lab3/Scripts/rectangel.py
+++ Labs/Lab3/Scripts/rectangel.py
@@ -1,25 +1,41 @@
 from geometry_shapes import GeometryShapes
 
 class Rectangel(GeometryShapes):
+    '''Class representing a rectangel'''
     def __init__(self, x: int | float, y: int | float, height: int|float, width: int|float):
+        '''Initialize a rectangel with given x and y coordinates, height and width'''
         super().__init__(x, y)
         self.height = height
         self.width = width
 
+    def __str__(self):
+        '''Return a string representation of the rectangel'''
+        return f"Rectangel: Postion (x, y): ({self.x, self.y}), Height: {self.height}, Width: {self.width}"
+    
+    def __repr__(self):
+        '''Return a string representation for developers'''
+        return f"Rectangel({self.x},{self.y},{self.height},{self.width})"
+
     def calculate_area(self):
+        '''Calcultate the area of the rectangel'''
         return self.height * self.width
     
     def circumference(self):
+        '''Calculate the circumference of the rectangel'''
         return self.height*2 + self.width*2
 
     @property
     def height(self):
+        '''Getter of height'''
         return self._height
     
     @height.setter
     def height(self, height: (int|float)):
+        '''Setter of height, with error handling'''
         if not isinstance(height, (int|float)):
-            raise TypeError(f"value of height can only be numeric.")
+            raise TypeError(f"Value of height can only be numeric. You wrote {height}.")
+        elif height <= 0:
+            raise ValueError(f"Height must be positive. You wrote {height}.")
         self._height = height
 
     @property
@@ -29,5 +45,7 @@ class Rectangel(GeometryShapes):
     @width.setter
     def width(self, width: (int|float)):
         if not isinstance(width, (int|float)):
-            raise TypeError(f"value of Width can only be numeric.")
+            raise TypeError(f"Value of Width can only be numeric. You wrote {width}.")
+        elif width <= 0:
+            raise ValueError(f"Width must be positive. You wrote {width}.")
         self._width = width
