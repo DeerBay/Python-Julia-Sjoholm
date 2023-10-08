9880145f57421d4a94b7386e2d4280407614cd5d
DeerBay <juliamariasjoholm@gmail.com>
2023-10-08 12:59:31 +0200

    add is unit circle method, add docstrings

--- Labs/Lab3/Scripts/circle.py
+++ Labs/Lab3/Scripts/circle.py
@@ -14,15 +14,22 @@ class Circle(GeometryShapes):
         return f"Circle({self.x}, {self.y}, {self.radius})"
     
     def calculate_area(self):
+        '''Calcultate the area of the circle. Returns float.'''
         import numpy as np
         return np.pi * self.radius * self.radius
 
     def circumference(self):
+        '''Calcultate the circumference of the circle. Returns float.'''
         import numpy as np
         return 2 * np.pi * self.radius
 
-    def is_unit_circle():
-        pass
+    def is_unit_circle(self):
+        '''calculates the squared distance from the center of the circle to the origin. Then, the method checks if this squared distance is equal to 1. If it is, the circle is a unit circle; otherwise, it is not.'''
+        distance_to_origin = (self.x - 0) ** 2 + (self.y - 0) ** 2
+        if distance_to_origin == 1:
+            print("This is a unit circle.")
+        else:
+            print("This is not a unit circle.")
 
     def is_point_inside():
         pass
