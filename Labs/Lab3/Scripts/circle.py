85f1a96ac800fd08ed64645368c0c71a29ee3ed3
DeerBay <juliamariasjoholm@gmail.com>
2023-10-08 12:29:00 +0200

    add setter, getter and prepare for other methods

--- Labs/Lab3/Scripts/circle.py
+++ Labs/Lab3/Scripts/circle.py
@@ -12,3 +12,29 @@ class Circle(GeometryShapes):
     def __repr__(self):
         '''Return a string representation for developers'''
         return f"Circle({self.x}, {self.y}, {self.radius})"
+    
+    def calculate_area(self):
+        pass
+
+    def circumference():
+        pass
+
+    def is_unit_circle():
+        pass
+
+    def is_point_inside():
+        pass
+
+    @property
+    def radius(self):
+        '''Getter of radius'''
+        return self._radius
+    
+    @radius.setter
+    def radius(self, radius):
+        '''Setter of height. Only positive numeric values is valid input.'''
+        if not isinstance(radius, (int, float)):
+            raise TypeError(f"Value of height can only be numeric. You wrote '{height}'.")
+        elif radius <= 0:
+            raise ValueError(f"Height must be positive. You wrote {height}.")
+        self._radius = radius
