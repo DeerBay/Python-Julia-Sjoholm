38d456565b8df15641e0dd3778773597cdba3277
DeerBay <juliamariasjoholm@gmail.com>
2023-10-08 18:32:59 +0200

    moved Rectangle, and Circle classes to geometry..

--- Labs/Lab3/Scripts/geometry_shapes.py
+++ Labs/Lab3/Scripts/geometry_shapes.py
@@ -70,3 +70,127 @@ class GeometryShapes:
     def __le__(self, other):
         '''Operator overload for less than or equal to.'''
         return self.calculate_area() <= other.calculate_area()
+
+
+class Rectangle(GeometryShapes):
+    '''Class representing a rectangle'''
+    def __init__(self, x: int | float, y: int | float, height: int|float, width: int|float):
+        '''Initialize a rectangle with given x and y coordinates, height and width.'''
+        super().__init__(x, y)
+        self.height = height
+        self.width = width
+
+    def __str__(self):
+        '''Return a string representation of the rectangle'''
+        return f"Rectangle: Postion (x, y): ({self.x, self.y}), Height: {self.height}, Width: {self.width}"
+    
+    def __repr__(self):
+        '''Return a string representation for developers'''
+        return f"Rectangle({self.x},{self.y},{self.height},{self.width})"
+
+    def calculate_area(self):
+        '''Calcultate the area of the rectangle. Return float.'''
+        return self.height * self.width
+    
+    def circumference(self):
+        '''Calculate the circumference of the rectangle. Returns float.'''
+        return self.height*2 + self.width*2
+    
+    def is_square(self):
+        '''Checks if the rectangel is a square.'''
+        if self.height == self.width:
+            return f"This rectangle is a square."
+        else:
+            return f"This rectangle is not a square."
+        
+    def is_point_inside(self, point_x: (int, float), point_y: (int, float)):
+        '''Checks if point (point_x, point_y) is inside rectangle. Returns bool (True/False).'''
+        left_boundary = self._x - self.width/2
+        right_boundary = self._x + self.width/2
+        top_boundary =  self._y + self.height/2
+        bottom_boundary = self._y - self.height/2
+
+        return left_boundary <= point_x <= right_boundary and  bottom_boundary <= point_y <= top_boundary
+
+    @property
+    def height(self):
+        '''Getter of height'''
+        return self._height
+    
+    @height.setter
+    def height(self, height):
+        '''Setter of height. Only positive numeric values is valid input.'''
+        if not isinstance(height, (int, float)):
+            raise TypeError(f"Value of height can only be numeric. You wrote '{height}'.")
+        elif height <= 0:
+            raise ValueError(f"Height must be positive. You wrote {height}.")
+        self._height = height
+
+    @property
+    def width(self):
+        '''Getter of width'''
+        return self._width
+    
+    @width.setter
+    def width(self, width: (int, float)):
+        '''Setter of width. Only positive numeric values is valid input.'''
+        if not isinstance(width, (int, float)):
+            raise TypeError(f"Value of width can only be numeric. You wrote '{width}'.")
+        elif width <= 0:
+            raise ValueError(f"Width must be positive. You wrote {width}.")
+        self._width = width
+
+
+class Circle(GeometryShapes):
+    '''Class representation of circle'''
+    def __init__(self, x: int | float, y: int | float, radius):
+        '''Initialize a circle with given x and y coordinates and radius.'''
+        super().__init__(x, y)
+        self.radius = radius
+
+    def __str__(self):
+        '''Return a string representation of the rectangle'''
+        return f"Circle: Postion (x, y): ({self.x, self.y}), Radius: {self.radius}."
+    
+    def __repr__(self):
+        '''Return a string representation for developers'''
+        return f"Circle({self.x}, {self.y}, {self.radius})"
+    
+    def calculate_area(self):
+        '''Calcultate the area of the circle. Returns float.'''
+        import numpy as np
+        return np.pi * (self.radius)**2
+
+    def circumference(self):
+        '''Calcultate the circumference of the circle. Returns float.'''
+        import numpy as np
+        return 2 * np.pi * self.radius
+
+    def is_unit_circle(self):
+        '''Checks if the circle is a unit circle with center at (0, 0) and radius of 1.'''
+        if self.x == 0 and self.y == 0 and self.radius == 1:
+            return True
+        else: 
+            return False
+
+    def is_point_inside(self, point_x: (int, float), point_y: (int, float)):
+        '''Checks if point (point_x, point_y) is inside circle. Returns bool (True/False).'''
+        distance_to_point = (self.x - point_x)**2 + (self.y - point_y)**2
+        if distance_to_point <= self.radius**2:
+            print(f"The point({point_x},{point_y}) is inside the circle.")
+        else:
+            print(f"The point({point_x},{point_y}) is not inside the circle.")
+
+    @property
+    def radius(self):
+        '''Getter of radius'''
+        return self._radius
+    
+    @radius.setter
+    def radius(self, radius):
+        '''Setter of height. Only positive numeric values is valid input.'''
+        if not isinstance(radius, (int, float)):
+            raise TypeError(f"Value of height can only be numeric. You wrote '{height}'.")
+        elif radius <= 0:
+            raise ValueError(f"Height must be positive. You wrote {height}.")
+        self._radius = radius
